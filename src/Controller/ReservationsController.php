cf2538819e3269d6f9d6af15feb2223cf52e60b4
Benoît Wallecan <benopen@gmail.com>
2016-01-09 00:18:18 +0100

    ReservationsController: initial implementation of addForm()
    It will be the public form for adding a reservation

--- src/Controller/ReservationsController.php
+++ src/Controller/ReservationsController.php
@@ -9,10 +9,25 @@ namespace Drupal\reservations\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class ReservationsController extends ControllerBase {
+
+
   public function content() {
     return array(
         '#type' => 'markup',
         '#markup' => $this->t('Hello, World!'),
     );
+  }
+  
+  /**
+   * Returns a form to add a new reservation.
+   *
+   * @return array
+   *   The reservation add form.
+   */
+  public function addForm() {
+    // TODO: implement getCurentSeason() and error page when not applicable
+    //$season = $this->entityManager()->getStorage('season')->getCurentSeason();
+    $reservation = $this->entityManager()->getStorage('reservation')->create(array('season' => '2015'));
+    return $this->entityFormBuilder()->getForm($reservation, 'add');
   }
 }
